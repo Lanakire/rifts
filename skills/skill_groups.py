8cb84e167f70519aad2659b8b8a6f25120385b52
chrislococo-wf <chris.lococo@webfilings.com>
2014-07-07 20:02:37 -0600

    Added the skills package.
    This package will contain the groups of skills as well as the
    individual skills that a character would have.

---
+++ skills/skill_groups.py
@@ -0,0 +1,19 @@
+
+class SkillGroup(object):
+    pass
+
+
+class Communication(SkillGroup):
+    pass
+
+
+class Cowboy(SkillGroup):
+    pass
+
+
+class Domestic(SkillGroup):
+    pass
+
+
+class Electrical(SkillGroup):
+    pass
